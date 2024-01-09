2c27e098db33a443ae8c2603dea6f0f1cb1a1ef4
mehrshad_Senei <mehrshadsensi@gmail.com>
2024-01-09 22:54:11 +0330

    need to make shuffle system first

--- card/models.py
+++ card/models.py
@@ -4,11 +4,11 @@ from django.db import models
 
 
 class NormalCoup (models.Model):
-    card_nam = models.CharField(max_length=30)
+    card_name = models.CharField(max_length=30)
     persian_name = models.CharField(max_length=30)
     active_ability = models.TextField(blank=True)
     passive_ability = models.TextField(blank=True)
     is_normal = models.BooleanField(default=True)
 
     def __str__(self):
-        return self.card_nam
+        return self.card_name
---
+++ game/views/views.py
@@ -0,0 +1,19 @@
+# from django.shortcuts import render
+
+# Create your views here.
+
+n = 3
+cards = {'commander': f'{n}', 'princess': f'{n}', 'assassin': f'{n}',
+         'nobleman': f'{n}', 'ambassador': f'{n}', 'inspector': f'{n}'}
+
+
+"""def assassin(x):
+    target = 'kazem'
+    if target == cards['princess']:
+        print(cards['princess'])
+        print("your safe baby")
+    else:
+        print(cards['princess'])
+        print('shine baaka')"""
+
+# first we need to make a shuffle system
